c8d3f0a46543694443726da0e9aae2acd1e1cdda
DiscoPad <mshagar@gmail.com>
2023-07-04 11:42:40 -0400

    ayooo

--- Test.py
+++ Test.py
@@ -1,3 +1,11 @@
+import numpy as np
 import imagej
-ij =imagej.init("2.5.0")
-print(ij.getVersion())
+import scyjava
+
+scyjava.config.add_options('-Xmx6g')
+ij=imagej.init('/home/greebo/Desktop/Apps/Fiji.app')
+print(ij.getApp().getInfo(True))
+array = np.random.rand(5,4,3)
+dataset = ij.py.to_java(array)
+
+print(dataset.shape)
